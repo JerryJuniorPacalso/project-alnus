18af0f1d47d1b7503f9b7b64277e18e43ed9759e
JerryJuniorPacalso <pacalsojerry@gmail.com>
2024-08-25 20:16:07 +0800

    17-welcome-screen - Add string localization

--- app/src/main/java/com/example/project_alnus/MainActivity.kt
+++ app/src/main/java/com/example/project_alnus/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    WelcomeScreen("Project-Alnus")
+                    WelcomeScreen(stringResource(id = R.string.project_alnus))
                 }
             }
         }
