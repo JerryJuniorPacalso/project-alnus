354356867a907094b675180345cdd1619a8da9a7
JerryJuniorPacalso <pacalsojerry@gmail.com>
2024-08-26 19:09:45 +0800

    Revert "15-sign-up-screen - Update string from Android to Project Alnus"
    This reverts commit 4b4e1b9cec1b85baf9bb1f3d0714e340314ee32d.

--- app/src/main/java/com/example/project_alnus/MainActivity.kt
+++ app/src/main/java/com/example/project_alnus/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Project Alnus")
+                    Greeting("Android")
                 }
             }
         }
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ProjectalnusTheme {
-        Greeting("Project Alnus")
+        Greeting("Android")
     }
 }
